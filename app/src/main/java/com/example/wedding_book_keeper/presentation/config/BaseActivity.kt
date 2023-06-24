efa1322229b1cdd1bd72451a12e558bb1e5608af
sa46lll <9812smh@naver.com>
2023-06-25 01:17:44 +0900

    feat: BaseActivity, BaseFragment 생성
    issue: #2

---
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/config/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.wedding_book_keeper.presentation.config
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) : AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+    }
+
+    protected fun showToastMessage(message: String) =
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
