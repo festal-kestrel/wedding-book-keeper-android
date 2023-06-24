74b80d63349a156b75be5df299f2b1540f60bbf5
sa46lll <9812smh@naver.com>
2023-06-25 01:28:14 +0900

    feat: DataBinding 사용하도록 변경
    issue: #2

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.wedding_book_keeper.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.wedding_book_keeper.R
+import com.example.wedding_book_keeper.databinding.ActivityMainBinding
+import com.example.wedding_book_keeper.presentation.config.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
