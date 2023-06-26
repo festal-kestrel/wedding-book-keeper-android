9c68d3c672bd19de26654d32f4236a841bd4be58
zzzjinwook <zzzjinwook@naver.com>
2023-06-26 15:53:46 +0900

    feat : 관계설정화면 이동버튼

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/GuestRelationsActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/GuestRelationsActivity.kt
@@ -1,18 +1,25 @@
 package com.example.wedding_book_keeper.presentation.view.guest
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.example.wedding_book_keeper.R
 import com.example.wedding_book_keeper.databinding.ActivityGuestRelationsBinding
 import com.example.wedding_book_keeper.databinding.ActivityLoginBinding
 import com.example.wedding_book_keeper.presentation.config.BaseActivity
 
-class GuestRelationsActivity : BaseActivity<ActivityGuestRelationsBinding>(R.layout.activity_guest_relations) {
+class GuestRelationsActivity :
+    BaseActivity<ActivityGuestRelationsBinding>(R.layout.activity_guest_relations) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_relations)
 
-
+        binding.btnGoGiftAmount.setOnClickListener {
+            Log.d("hong","버튼 눌림")
+            val intent = Intent(this, GiftAmountActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
