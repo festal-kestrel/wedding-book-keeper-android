267bb284b67776f850828c70e6c88595bbd7ca50
sa46lll <azure394@gmail.com>
2023-06-26 15:00:15 +0900

    feat: 버튼 이벤트 설정 및 ui 수정
    issue: #5

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/wedding/schedule/ScheduleActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/wedding/schedule/ScheduleActivity.kt
@@ -1,15 +1,18 @@
 package com.example.wedding_book_keeper.presentation.view.wedding.schedule
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import com.example.wedding_book_keeper.R
 import com.example.wedding_book_keeper.databinding.ActivityScheduleBinding
 import com.example.wedding_book_keeper.presentation.config.BaseActivity
+import com.example.wedding_book_keeper.presentation.view.wedding.introduction.IntroductionActivity
 
-@RequiresApi(Build.VERSION_CODES.O)
+
 class ScheduleActivity : BaseActivity<ActivityScheduleBinding>(R.layout.activity_schedule) {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,7 +24,12 @@ class ScheduleActivity : BaseActivity<ActivityScheduleBinding>(R.layout.activity
         binding.txtSelectedDate.text = "${binding.dpWeddingDate.year}년 ${binding.dpWeddingDate.month + 1}월 ${binding.dpWeddingDate.dayOfMonth}일"
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun initEvent() {
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this, IntroductionActivity::class.java);
+            startActivity(intent)
+        }
         binding.dpWeddingDate.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
             binding.txtSelectedDate.text = "${year}년 ${monthOfYear + 1}월 ${dayOfMonth}일"
         }
