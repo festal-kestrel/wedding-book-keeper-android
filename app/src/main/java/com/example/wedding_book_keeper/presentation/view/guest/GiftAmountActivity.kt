b4da61bd5e4cec28aaef73626f8ba5cde9220e9c
zzzjinwook <zzzjinwook@naver.com>
2023-06-26 14:52:52 +0900

    fix : 숫자포맷팅

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/GiftAmountActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/GiftAmountActivity.kt
@@ -1,15 +1,68 @@
 package com.example.wedding_book_keeper.presentation.view.guest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import com.example.wedding_book_keeper.R
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.widget.EditText
 import com.example.wedding_book_keeper.databinding.ActivityGiftAmountBinding
-import com.example.wedding_book_keeper.databinding.ActivityLoginBinding
 import com.example.wedding_book_keeper.presentation.config.BaseActivity
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 
-class GiftAmountActivity : BaseActivity<ActivityGiftAmountBinding>(R.layout.activity_gift_amount) {
+
+class GiftAmountActivity :
+    BaseActivity<ActivityGiftAmountBinding>(com.example.wedding_book_keeper.R.layout.activity_gift_amount) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gift_amount)
+        setContentView(com.example.wedding_book_keeper.R.layout.activity_gift_amount)
+
+
+        val editText = findViewById<EditText>(com.example.wedding_book_keeper.R.id.edit_gift)
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                editText.removeTextChangedListener(this)
+                try {
+                    var originalString = s.toString()
+
+                    // Check if '원' is in the string
+                    if (originalString.contains("원")) {
+                        originalString = originalString.replace("원", "")
+                    }
+
+                    // Remove previous formatting character
+                    val cleanString = originalString.replace(",", "")
+
+                    // Parse cleanString as long value
+                    val parsed = cleanString.toLong()
+
+                    // Format as currency
+                    val formatter: DecimalFormat =
+                        NumberFormat.getInstance(Locale.getDefault()) as DecimalFormat
+                    formatter.applyPattern("#,###")
+                    var formatted: String = formatter.format(parsed)
+
+                    // Append '원' unit
+                    formatted += "원"
+
+                    // Set text
+                    editText.setText(formatted)
+
+                    // Set selection at the end of text
+                    editText.setSelection(editText.text.length - 1)
+                } catch (e: NumberFormatException) {
+                    e.printStackTrace()
+                }
+                editText.addTextChangedListener(this)
+            }
+        })
+
+
     }
 }
