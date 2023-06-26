e7b90bfc28a2afc482a326d2deec09a396dffe68
zzzjinwook <zzzjinwook@naver.com>
2023-06-26 15:10:18 +0900

    feat : 하객플로우 완료화면

---
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/GIftCompleteActivity.kt
@@ -0,0 +1,15 @@
+package com.example.wedding_book_keeper.presentation.view.guest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wedding_book_keeper.R
+import com.example.wedding_book_keeper.databinding.ActivityGiftCompleteBinding
+import com.example.wedding_book_keeper.databinding.ActivityGuestRelationsBinding
+import com.example.wedding_book_keeper.presentation.config.BaseActivity
+
+class GIftCompleteActivity : BaseActivity<ActivityGiftCompleteBinding>(R.layout.activity_gift_complete) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gift_complete)
+    }
+}
