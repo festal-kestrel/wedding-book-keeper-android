27e91ea6228f673160aff0318e683930f0b5b4ca
서명현 <azure394@gmail.com>
2023-06-27 11:35:43 +0900

    feat: 결혼식 정보 입력 UI 구현 (#11)
    * feat: 배우자 연결 ui 구현
    
    issue: #5
    
    * feat: 인증코드 DialogFragment 생성
    
    issue: #5
    
    * feat: 결혼식 일정 ui 구현
    
    issue: #5
    
    * feat: DialogFragment 속성 변경
    
    issue: #5
    
    * feat: 앱 소개 ui 구조 구현
    
    issue: #5
    
    * feat: ConstraintLayout으로 변경 및 버튼 추가
    
    issue: #5
    
    * docs: 이미지 수정
    
    * feat: 버튼 이벤트 설정 및 ui 수정
    
    issue: #5
    
    * feat: 인증코드 다이얼로그 ui 구현
    
    issue: #5

---
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/wedding/introduction/IntroductionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wedding_book_keeper.presentation.view.wedding.introduction
+
+import android.os.Bundle
+import com.example.wedding_book_keeper.R
+import com.example.wedding_book_keeper.databinding.ActivityIntroductionBinding
+import com.example.wedding_book_keeper.presentation.config.BaseActivity
+
+class IntroductionActivity : BaseActivity<ActivityIntroductionBinding>(R.layout.activity_introduction) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
