e8aed63ff6cad439c956e92107b12a4fc459cc69
zzzjinwook <zzzjinwook@naver.com>
2023-06-28 16:03:46 +0900

    feat : 관리자 코드 복사 기능

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/mypage/AdminCodeActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/mypage/AdminCodeActivity.kt
@@ -1,8 +1,12 @@
 package com.example.wedding_book_keeper.presentation.view.mypage
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.wedding_book_keeper.R
 import com.example.wedding_book_keeper.databinding.ActivityAdminCodeBinding
 import com.example.wedding_book_keeper.databinding.ActivityMyPageBinding
@@ -15,7 +19,18 @@ class AdminCodeActivity : BaseActivity<ActivityAdminCodeBinding>(R.layout.activi
         binding.btnToolbarBack.setOnClickListener {
             finish()
         }
+
+        binding.btnCopyCode.setOnClickListener{
+            val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val adminCode = binding.txtAdminCode.text.toString()
+            val clipData = ClipData.newPlainText("admin_code", adminCode)
+            clipboardManager.setPrimaryClip(clipData)
+
+            showToast("복사 완료")
+        }
     }
 
-
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
