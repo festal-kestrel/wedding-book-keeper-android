4d2dee01e2fb5f4a7b1773a25705a464b9917874
서명현 <azure394@gmail.com>
2023-06-30 12:32:00 +0900

    feat: retrofit 세팅 및 필요한 라이브러리 설치 (#23)
    * feat: 필요한 dependency 추가
    
    * feat: retrofit 세팅
    
    issue: #20
    
    * fix: 클래스명 변경
    
    * feat: 결혼 정보 조회 및 Retrofit Test 
    
    issue: #20
    
    * style: EOF 추가
    
    * refactor: 클래스명 변경

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.wedding_book_keeper.presentation.view
 
 import android.os.Bundle
+import android.util.Log
 import com.example.wedding_book_keeper.R
+import com.example.wedding_book_keeper.data.remote.WeddingBookKeeperClient
+import com.example.wedding_book_keeper.data.remote.response.WeddingInfoResponse
 import com.example.wedding_book_keeper.databinding.ActivityMainBinding
 import com.example.wedding_book_keeper.presentation.config.BaseActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private val TAG = this.javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initRetrofitTest()
+    }
+
+    private fun initRetrofitTest() {
+        WeddingBookKeeperClient.weddingService.getWeddingInfo(3).enqueue(object : Callback<WeddingInfoResponse> {
+            override fun onResponse(call: Call<WeddingInfoResponse>, response: Response<WeddingInfoResponse>) {
+                if (response.isSuccessful) {
+                    Log.d(TAG, "onResponse: ${response.body()}")
+                    showToastMessage("성공")
+
+                    // WeddingInfoResponse의 groomName, brideName 조회
+                    Log.d(TAG, "groomName: ${response.body()?.groomName}")
+                    Log.d(TAG, "brideName: ${response.body()?.brideName}")
+                    return;
+                }
+                showToastMessage("실패")
+            }
+
+            override fun onFailure(call: Call<WeddingInfoResponse>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+                showToastMessage("실패")
+            }
+        })
     }
 }
