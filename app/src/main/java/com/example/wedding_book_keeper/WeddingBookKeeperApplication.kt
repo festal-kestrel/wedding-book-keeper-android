f64f968d2640f33be64111e75dcbdfc2d75eca93
Sangwon <sangcho617@gmail.com>
2023-07-01 17:34:48 +0900

    feat: jwt 토큰 저장, interceptor 적용

--- app/src/main/java/com/example/wedding_book_keeper/WeddingBookKeeperApplication.kt
+++ app/src/main/java/com/example/wedding_book_keeper/WeddingBookKeeperApplication.kt
@@ -1,15 +1,17 @@
 package com.example.wedding_book_keeper
 
 import android.app.Application
+import com.example.wedding_book_keeper.presentation.config.Prefs
 import com.kakao.sdk.common.KakaoSdk
-import dagger.hilt.android.HiltAndroidApp
 
 //@HiltAndroidApp
 class WeddingBookKeeperApplication : Application() {
-
+    companion object{
+        lateinit var prefs: Prefs
+    }
     override fun onCreate() {
         super.onCreate()
-
+        prefs=Prefs(applicationContext)
         // KaKao SDK  초기화
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/LoginActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/guest/LoginActivity.kt
@@ -12,17 +12,18 @@ import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.wedding_book_keeper.WeddingBookKeeperApplication
+import com.example.wedding_book_keeper.data.remote.WeddingBookKeeperClient
+import com.example.wedding_book_keeper.presentation.view.MainActivity
 import com.example.wedding_book_keeper.presentation.view.donation.guest.GuestMainActivity
 import com.example.wedding_book_keeper.presentation.view.guest.api.LoginService
 import com.example.wedding_book_keeper.presentation.view.guest.dto.TokenResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
-    private lateinit var loginService: LoginService
+    private val TAG = "LOGIN"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +34,18 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
 
             val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
                 if (error != null) {
-                    Log.e("LOGIN", "카카오계정으로 로그인 실패", error)
+                    Log.e(TAG, "카카오계정으로 로그인 실패", error)
                 } else if (token != null) {
+//                    val intent = Intent(this, MainActivity::class.java)
                     val intent = Intent(this, GuestMainActivity::class.java)
+                    Log.i(TAG, "카카오계정으로 로그인 성공, 카카오토큰 == ${token.accessToken}")
+                    try {
+                        sendMemberToken(token.accessToken)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
                     startActivity(intent)
                     finish()
-                    sendMemberToken(token.accessToken)
-                    Log.i("LOGIN", "카카오계정으로 로그인 성공 ${token.accessToken}")
                 }
             }
 
@@ -47,18 +53,16 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
             if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
                 UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                     if (error != null) {
-                        Log.e("LOGIN", "카카오톡으로 로그인 실패", error)
-
+                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
                         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                         if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
                             return@loginWithKakaoTalk
                         }
-
                         // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
                         UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
                     } else if (token != null) {
-                        Log.i("LOGIN", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
                         val intent = Intent(this, GuestMainActivity::class.java)
                         startActivity(intent)
                         finish()
@@ -71,23 +75,15 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
     }
 
     private fun sendMemberToken(token: String) {
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        loginService = retrofit.create(LoginService::class.java)
-        val call = loginService.authorize(token)
-
-        call.enqueue(object : Callback<TokenResponse> {
+        WeddingBookKeeperClient.loginService.authorize(token).enqueue(object : Callback<TokenResponse> {
             override fun onResponse(call: Call<TokenResponse>, response: Response<TokenResponse>) {
                 if (response.isSuccessful) {
                     val result: TokenResponse? = response.body()
-                    // TODO: JWT 토큰 로컬에 저장
-                    Log.d("LOGIN", "jwtToken============= ${result}")
+                    WeddingBookKeeperApplication.prefs.token=result?.jwtToken
+                    Log.d(TAG, "서버에서 받아온 jwtToken 값 == $result")
+                    Log.d(TAG, "SHAREDPREFS에 저장된 토큰 값 == ${WeddingBookKeeperApplication.prefs.token}")
                 } else {
-                    Log.d("LOGIN", "${response}")
+                    Log.d(TAG, "$response")
                 }
             }
 
