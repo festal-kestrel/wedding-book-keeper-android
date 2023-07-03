e4dcc1fb7ce68d49e8a57d62aa6c2f9335b141e8
JOOWON <dev.joowon@gmail.com>
2023-07-03 09:18:09 +0900

    feat: 마이페이지 결혼식 정보 수정 api 연결

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/mypage/WeddingDatePickerActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/mypage/WeddingDatePickerActivity.kt
@@ -1,11 +1,25 @@
 package com.example.wedding_book_keeper.presentation.view.mypage
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import com.example.wedding_book_keeper.R
+import com.example.wedding_book_keeper.data.remote.WeddingBookKeeperClient
+import com.example.wedding_book_keeper.data.remote.request.WeddingUpdateInformationRequest
+import com.example.wedding_book_keeper.data.remote.response.WeddingInfoResponse
 import com.example.wedding_book_keeper.databinding.ActivityWeddingDatePickerBinding
 import com.example.wedding_book_keeper.presentation.config.BaseActivity
+import com.example.wedding_book_keeper.presentation.view.donation.couple.CoupleMainActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class WeddingDatePickerActivity : BaseActivity<ActivityWeddingDatePickerBinding>(R.layout.activity_wedding_date_picker) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +28,66 @@ class WeddingDatePickerActivity : BaseActivity<ActivityWeddingDatePickerBinding>
         binding.btnToolbarBack.setOnClickListener {
             finish()
         }
+
+        binding.btnToolbarConfirm.setOnClickListener {
+
+            val selectedDate = getSelectedDate()
+            val selectedTime = getSelectedTime()
+
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+            val formattedDate = dateFormat.format(selectedDate)
+            val formattedTime = timeFormat.format(selectedTime)
+            println("Selected Date: $formattedDate")
+            println("Selected Time: $formattedTime")
+
+            var newLocation = binding.txtNewLocation.text.toString()
+            var newDateTime = formattedDate + " " + formattedTime
+
+            updateWeddingInfo(90, WeddingUpdateInformationRequest(location = newLocation, newDateTime))
+
+            var intent = Intent(this, CoupleMyPageActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
+    private fun getSelectedDate(): Date {
+        val year = binding.datePicker.year
+        val month = binding.datePicker.month
+        val dayOfMonth = binding.datePicker.dayOfMonth
+
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        return calendar.time
+    }
+
+    private fun getSelectedTime(): Date {
+        val hour = binding.timePicker.hour
+        val minute = binding.timePicker.minute
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hour)
+        calendar.set(Calendar.MINUTE, minute)
+        return calendar.time
+    }
+
+
+    private fun updateWeddingInfo(weddingId: Int, weddingUpdateInformationRequest: WeddingUpdateInformationRequest) {
+        WeddingBookKeeperClient.weddingService.updateWeddingInfo(weddingId, weddingUpdateInformationRequest)
+            .enqueue(object : Callback<Unit> {
+                override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        body?.let {
+                            showToastMessage("성공")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<Unit>, t: Throwable) {
+                    // handle error
+                }
+            })
+    }
 }
