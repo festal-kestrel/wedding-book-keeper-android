16a7d0fcd76f4f5769759e3bb071f1869ffd924e
zzzjinwook <zzzjinwook@naver.com>
2023-07-03 17:11:33 +0900

    feat : qr이미지 로컬 저장소에 저장

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/mypage/GuestEntryQRActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/mypage/GuestEntryQRActivity.kt
@@ -1,17 +1,21 @@
 package com.example.wedding_book_keeper.presentation.view.mypage
 
+import android.content.ContentValues
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import androidx.databinding.DataBindingUtil.setContentView
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.wedding_book_keeper.R
 import com.example.wedding_book_keeper.data.remote.WeddingBookKeeperClient
-import com.example.wedding_book_keeper.data.remote.response.WeddingInfoResponse
 import com.example.wedding_book_keeper.data.remote.response.WeddingQrResponse
 import com.example.wedding_book_keeper.databinding.ActivityGuestEntryQractivityBinding
-import com.example.wedding_book_keeper.databinding.ActivityWeddingDatePickerBinding
 import com.example.wedding_book_keeper.presentation.config.BaseActivity
 import retrofit2.Call
 import retrofit2.Callback
@@ -25,6 +29,10 @@ class GuestEntryQRActivity : BaseActivity<ActivityGuestEntryQractivityBinding>(R
             finish()
         }
         getWeddingQr(90)
+
+        binding.textSaveQrBtn.setOnClickListener {
+            saveImageToGallery()
+        }
     }
 
     private fun getWeddingQr(weddingId: Int) {
@@ -58,7 +66,39 @@ class GuestEntryQRActivity : BaseActivity<ActivityGuestEntryQractivityBinding>(R
         Glide.with(this)
             .load(qrImgUrl)
             .into(binding.imgQr)
-
     }
 
+    private fun saveImageToGallery() {
+        val sharedPref = getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+        val qrImgUrl = sharedPref.getString("qrImgUrl", null)
+
+        Glide.with(this)
+            .asBitmap()
+            .load(qrImgUrl)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    val resolver = contentResolver
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, "QR Image.jpg")
+                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                        put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/")
+                    }
+
+                    val uri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                    uri?.let {
+                        resolver.openOutputStream(it)?.use { outputStream ->
+                            if (!resource.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
+                                Toast.makeText(this@GuestEntryQRActivity, "QR코드를 갤러리에 저장했습니다.", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this@GuestEntryQRActivity, "QR코드 저장에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    // This method is intentionally empty
+                }
+            })
+    }
 }
