1ff575a3e79df51de0b0271bd6650a9d14e4f177
zzzjinwook <zzzjinwook@naver.com>
2023-07-03 13:42:34 +0900

    feat : 관리자 화면 검색기능 추가

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/donation/manager/ManagerMainDonationAdapter.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/donation/manager/ManagerMainDonationAdapter.kt
@@ -8,8 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.wedding_book_keeper.R
 import com.example.wedding_book_keeper.presentation.view.donation.couple.GuestDonationInfo
 
-class ManagerMainDonationAdapter(val guestList: MutableList<GuestDonationInfo>) :
+class ManagerMainDonationAdapter(var guestList: MutableList<GuestDonationInfo>) :
     RecyclerView.Adapter<ManagerMainDonationAdapter.CustomViewHolder>() {
+
+    private var originalList: List<GuestDonationInfo> = guestList.toList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.main_item_manager, parent, false)
@@ -37,7 +40,22 @@ class ManagerMainDonationAdapter(val guestList: MutableList<GuestDonationInfo>) 
         val donationDate = itemView.findViewById<TextView>(R.id.txt_donation_date)
     }
 
+    // 새로운 아이템 세팅 함수에서도 원본 데이터를 갱신해줍니다.
     fun setItems(items: MutableList<GuestDonationInfo>) {
-        guestList.addAll(items)
+        this.guestList = items
+        this.originalList = items.toList()
+        notifyDataSetChanged()
+    }
+
+    fun filter(filterText: String) {
+        guestList = if (filterText.isEmpty()) {
+            originalList.toMutableList()
+        } else {
+            originalList.filter {
+                it.guestName.contains(filterText, ignoreCase = true)
+                // 필요하다면 다른 필터링 조건도 추가할 수 있습니다.
+            }.toMutableList()
+        }
+        notifyDataSetChanged() // 데이터가 변경되었음을 어댑터에 알림
     }
 }
