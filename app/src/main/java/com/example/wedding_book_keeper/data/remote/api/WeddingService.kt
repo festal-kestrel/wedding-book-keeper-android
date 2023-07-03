414ca67bf8257055acde7f10d32f4838167be441
JOOWON <dev.joowon@gmail.com>
2023-07-03 09:13:34 +0900

    feat: 마이페이지 결혼식 정보 api 연결

--- app/src/main/java/com/example/wedding_book_keeper/data/remote/api/WeddingService.kt
+++ app/src/main/java/com/example/wedding_book_keeper/data/remote/api/WeddingService.kt
@@ -1,8 +1,14 @@
 package com.example.wedding_book_keeper.data.remote.api
 
+import com.example.wedding_book_keeper.data.remote.request.WeddingUpdateInformationRequest
 import com.example.wedding_book_keeper.data.remote.response.WeddingInfoResponse
+import com.example.wedding_book_keeper.data.remote.response.WeddingManagerCodeResponse
+import com.example.wedding_book_keeper.data.remote.response.WeddingQrResponse
 import retrofit2.Call;
+import retrofit2.http.Body
 import retrofit2.http.GET;
+import retrofit2.http.PATCH
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface WeddingService {
@@ -11,4 +17,21 @@ interface WeddingService {
     fun getWeddingInfo(
         @Path("weddingId") weddingId: Int
     ): Call<WeddingInfoResponse>
+
+    @GET("weddings/{weddingId}/qr")
+    fun getWeddingQr(
+        @Path("weddingId") weddingId: Int
+    ): Call<WeddingQrResponse>
+
+    @GET("weddings/{weddingId}/admin/code")
+    fun getManagerCode(
+        @Path("weddingId") weddingId: Int
+    ): Call<WeddingManagerCodeResponse>
+
+    @PATCH("weddings/{weddingId}")
+    fun updateWeddingInfo(
+        @Path("weddingId") weddingId: Int,
+        @Body weddingUpdateInformationRequest: WeddingUpdateInformationRequest
+    ): Call<Unit>
+
 }
