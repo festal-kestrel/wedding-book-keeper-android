7e38a02e93e92b5da996b2251b8cead2c432f13b
Sangwon <40656716+swc617@users.noreply.github.com>
2023-07-04 11:19:53 +0900

    jwt 토큰 저장, interceptor 적용 (#28)
    * feat: jwt 토큰 저장, interceptor 적용
    
    * feat: 자동 로그인 기능 구현

--- app/src/main/java/com/example/wedding_book_keeper/data/remote/WeddingBookKeeperClient.kt
+++ app/src/main/java/com/example/wedding_book_keeper/data/remote/WeddingBookKeeperClient.kt
@@ -2,6 +2,8 @@ package com.example.wedding_book_keeper.data.remote
 
 import com.example.wedding_book_keeper.data.remote.api.AuthService
 import com.example.wedding_book_keeper.data.remote.api.WeddingService
+import com.example.wedding_book_keeper.presentation.config.AuthInterceptor
+import com.example.wedding_book_keeper.presentation.view.guest.api.LoginService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -9,26 +11,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object WeddingBookKeeperClient {
 
-    private val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    private const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(AuthInterceptor())
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     val weddingService: WeddingService by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         provideApiRetrofit(WeddingService::class.java)
     }
 
+
+    val loginService: LoginService by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        provideApiRetrofit(LoginService::class.java)
+
     val authService: AuthService by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         provideApiRetrofit(AuthService::class.java)
+
     }
 
     private fun <T> provideApiRetrofit(clazz: Class<T>): T = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .client(provideHttpLoggingClient())
+        .client(okHttpClient)
         .build()
         .create(clazz)
 
-    private fun provideHttpLoggingClient(): OkHttpClient =
-        HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }.let {
-            OkHttpClient.Builder().addInterceptor(it).build() }
 }
