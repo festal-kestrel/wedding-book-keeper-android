c9fccd92a9fe5d979a38054b191c3939cf17d644
sa46lll <azure394@gmail.com>
2023-07-04 09:55:35 +0900

    feat: wedding timePicker 추가

--- app/src/main/java/com/example/wedding_book_keeper/presentation/view/wedding/schedule/ScheduleActivity.kt
+++ app/src/main/java/com/example/wedding_book_keeper/presentation/view/wedding/schedule/ScheduleActivity.kt
@@ -21,17 +21,25 @@ class ScheduleActivity : BaseActivity<ActivityScheduleBinding>(R.layout.activity
     }
 
     private fun initView() {
-        binding.txtSelectedDate.text = "${binding.dpWeddingDate.year}년 ${binding.dpWeddingDate.month + 1}월 ${binding.dpWeddingDate.dayOfMonth}일"
+        binding.txtSelectedDate.text =
+            "${binding.dpWeddingDate.year}년 ${binding.dpWeddingDate.month + 1}월 ${binding.dpWeddingDate.dayOfMonth}일"
+        binding.txtSelectedTime.text = "${binding.tpWeddingTime.hour}시 ${binding.tpWeddingTime.minute}분"
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initEvent() {
         binding.btnNext.setOnClickListener {
             val intent = Intent(this, IntroductionActivity::class.java);
+            intent.putExtra("weddingDate", "${binding.dpWeddingDate.year}-${binding.dpWeddingDate.month + 1}-${binding.dpWeddingDate.dayOfMonth} ${binding.tpWeddingTime.hour}")
             startActivity(intent)
         }
+
         binding.dpWeddingDate.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
             binding.txtSelectedDate.text = "${year}년 ${monthOfYear + 1}월 ${dayOfMonth}일"
+        }
+
+        binding.tpWeddingTime.setOnTimeChangedListener { view, hourOfDay, minute ->
+            binding.txtSelectedTime.text = "오후 ${hourOfDay}시 ${minute}분"
         }
     }
 }
